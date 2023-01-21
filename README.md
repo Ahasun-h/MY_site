1206d47dd26f6b39ac2be7302491ba917655f704
niloy-d <niloydaskuet@gmail.com>
2023-01-21 19:13:12 +0600

    First Commit

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-To view this unstyled markup visit [http://ahasun180.000webhostapp.com/#Services](http://ahasun180.000webhostapp.com/#Services)
-
-# MY Site - Static One Page Personal Portfolio Website
-
-It's a static one-page portfolio website. There you get Home, Services, Resume, etc. Here 'About Me'  section explains about you. Fully workable contact form, which redirects email to your wanted email.
-
-![Screenshot 2022-05-09 at 04-49-00 Ahasun_H](https://user-images.githubusercontent.com/33959275/167319117-ea060134-15ed-4f10-8ac8-50f420e6cc39.png)
+# Personal One Page Website
 
 # Features
 
@@ -17,12 +11,10 @@ It's a static one-page portfolio website. There you get Home, Services, Resume, 
 
 * Create a new repo for your project on Github
 * In terminal run
-
 ```bash
     git clone git@github.com:mrmrs/html.git yourNewRepoName
     cd [yourNewRepoName]
     git remote rm origin
-
     git remote add origin git@github.com:yourUserName/yourNewRepoName.git
     git remote -v
 ```
@@ -36,11 +28,7 @@ It's a static one-page portfolio website. There you get Home, Services, Resume, 
 Now you're set up to use this in a new project.
 
 # Getting Started
-* Setup contact form mail -> open your-project/contact.php in your favorite text editor.
-*change 'example@email.com' in line no:37 with your email.
 
 ## The HTML
 * Open up index.html with your favorite text editor and hack away. It's that simple.
 * TTo preview changes open index.html with your favorite browser.
-
-# Author
--- contact.php
+++ contact.php
@@ -1,47 +1,67 @@
-<?php 
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
 
-    if(isset($_POST['submit'])){
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
 
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
+$mail = new PHPMailer(true);
 
-        $mailBody = "Message From My Portfolio Site. \n <table>
-                                                            <tr>
-                                                                <td>Name:</td>
-                                                                <td>$name</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>Subject:</td>
-                                                                <td>$subject</td>
-                                                            </tr>
-                                                            <tr>
-                                                                <td>Message:</td>
-                                                                <td>$message</td>
-                                                            </tr>
-                                                        </table>";
+$alert = '';
 
-        $mailheader ="From : $email \r\n";
-        $mailheader = "Reply-To: " .$name.  "\r\n";
-        $mailheader = "CC: susan@example.com\r\n";
-        $mailheader = "MIME-Version: 1.0\r\n";
-        $mailheader = "Content-Type: text/html; charset=UTF-8\r\n";
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'ahasun180.000webhostapp.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'habibahasun019@gmail.com'; // Gmail address Password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
 
-        if( empty($name) || empty($email) || empty($subject) ||empty($message)  )
-        {
-            header('location:index.php?error');
-        }
-        else{
-            $to = "habibahasun019@gmail.com";
+    $mail->setFrom('ahasun180.000webhostapp.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('habibahasun019@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
-            if( mail( $to,$subject,$mailBody,$mailheader ) ){
-                header('location:index.php?success');
-            }
-        }
-    }else{
-        header('location:index.php');
-    }
+    $mail->isHTML(true);
+    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Body = "<h3>Name : $name <br>Email: $email <br>Subject: $subject<br>Message : $message</h3>";
 
+    $mail->send();
+    $alert = '<div aria-live="polite" aria-atomic="true" style="position: relative; min-height: 200px;">
+    <div class="toast" style="position: absolute; top: 0; right: 0;">
+        <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">Bootstrap</strong>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="toast-body">
+        Message Sent! Thank you for contacting me.
+        </div>
+    </div>
+</div>';
+  } catch (Exception $e){
+    $alert = '
+              
+              <div class="toast" style="position: absolute; top: 0; right: 0;">
+        <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="...">
+            <strong class="mr-auto">Bootstrap</strong>
+            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        </div>
+        <div class="toast-body">
+       '.$e->getMessage().'
+        </div>
+    </div>
+              ';
+  }
+}
 ?>
